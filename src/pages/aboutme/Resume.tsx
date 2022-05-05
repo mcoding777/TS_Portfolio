4c8e94567755bbc759833534c4b021132365ba32
misun <mcoding777@naver.com>
2022-05-05 13:28:56 +0900

    Style: Resume area file cleanup

--- src/pages/aboutme/Resume.tsx
+++ src/pages/aboutme/Resume.tsx
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import { SubTitle, Section } from '../../components';
-import ResumeCard from '../../components/aboutme/ResumeCard';
 import React from 'react';
 
 const Resume: React.FC = (): JSX.Element => {
-    const tmpTimeLine = [
+    const resumeDetail = [
         {
             year: 2019,
             detail: [
@@ -49,17 +48,106 @@ const Resume: React.FC = (): JSX.Element => {
     return (
         <Section>
             <SubTitle text="💼 Resume" />
-            <ResumeCardArea>
-                <ResumeCard title="🕰 TimeLine" resumeDetail={tmpTimeLine} isEditMode={false} />
-                <ResumeCard title="👑 Certificate & Awards" resumeDetail={tmpTimeLine} isEditMode={false} />
-            </ResumeCardArea>
+            <ResumeDiv>
+                <ResumeCardDiv>
+                    <CardDetailArea>
+                        {resumeDetail.map((item, idx) => {
+                            return (
+                                <DetailRowContainer className="test" key={idx}>
+                                    <YearTitle>{item.year}</YearTitle>
+                                    <YearColumnDiv>
+                                        {item.detail.map((i, idx) => (
+                                            <YearRowDiv key={idx}>
+                                                <YearDot />
+                                                <div>
+                                                    <DetailTitle>{i.detailTitle}</DetailTitle>
+                                                    <DetailDescribtion>{i.detailDescriptions}</DetailDescribtion>
+                                                </div>
+                                            </YearRowDiv>
+                                        ))}
+                                    </YearColumnDiv>
+                                </DetailRowContainer>
+                            );
+                        })}
+                    </CardDetailArea>
+                </ResumeCardDiv>
+            </ResumeDiv>
         </Section>
     );
 };
 
 export default Resume;
 
-const ResumeCardArea = styled.div`
+const ResumeDiv = styled.div`
     display: flex;
     flex-direction: row;
+`;
+
+const ResumeCardDiv = styled.div`
+    width: 100%;
+    margin: 20px 5px;
+`;
+
+const CardDetailArea = styled.div`
+    min-height: 400px;
+    padding: 15px 10px;
+    box-sizing: border-box;
+    background-color: rgba(196, 196, 196, 0.4);
+    border-radius: 0px 0px 8px 8px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+`;
+
+const DetailRowContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const YearTitle = styled.li`
+    width: 50px;
+    margin-bottom: 20px;
+    padding: 0;
+    list-style: none;
+    font-size: 19px;
+    font-weight: 600;
+`;
+
+const YearColumnDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 22px;
+`;
+
+const YearRowDiv = styled.div`
+    width: 80%;
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    border-radius: 8px;
+    background-color: #fff;
+    margin: 2px 0px;
+    padding: 5px 3px;
+    box-sizing: border-box;
+`;
+
+const YearDot = styled.div`
+    background-color: ${(props) => props.theme.color.main};
+    width: 15px;
+    height: 15px;
+    border-radius: 15px;
+    margin: 0px 5px 0px 10px;
+`;
+
+const DetailTitle = styled.div`
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 2px;
+`;
+
+const DetailDescribtion = styled.div`
+    font-size: 14px;
+    font-weight: 300;
+    margin-bottom: 10px;
 `;
